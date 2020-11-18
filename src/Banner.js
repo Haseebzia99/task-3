048ad08bf05bf11f9f563b8aebdf9905a8b4be24
Haseeb Zia <haseebzia99@outlook.com>
2020-11-18 17:37:23 +0000

    task 3

---
+++ src/Banner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Banner.css'
+
+const Banner = () => {
+    return(
+        <div className="banner">
+            <div className="banner-text">
+                <h1> FIND YOUR MOVIE </h1>
+            </div>
+        </div>
+    )
+}
+
+export default Banner;
