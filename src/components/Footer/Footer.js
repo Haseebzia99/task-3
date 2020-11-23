91d588604df01943335c6271e349746087886a28
Haseeb Zia <haseebzia99@outlook.com>
2020-11-23 14:57:58 +0000

    lifecycle method

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,30 @@
-import React from 'react'
 import './Footer.css'
+import React, { Component } from 'react'
 
-export const Footer = () => {
-    return (
-        <div class="footer">
-            <img className="logo" src={"https://www.freepnglogos.com/uploads/netflix-logo-0.png"}/>
-        </div>
-    )
+class Footer extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            date: new Date(),
+        };
+    }
+    
+    tick = () => {
+        this.setState({
+            date: new Date()
+        })
+    }
+
+    render () {
+        return (
+            <div class="footer">
+            <h2> {this.state.date.toLocaleTimeString()} </h2>
+            <button onClick={this.tick}>Update</button>
+             <img className="logo" src={"https://www.freepnglogos.com/uploads/netflix-logo-0.png"}/>
+            </div>
+        );
+    }
 }
 
-export default Footer;
+export default Footer
